f73e7c40b57a83f57d0f89d241d07e1929a72a5c
Your Name <your.email@example.com>
2026-04-03 23:04:20 +0800

    update daily python practice

--- docs/code/Python basic/Python_learning/function_practice.py
+++ docs/code/Python basic/Python_learning/function_practice.py
@@ -3,14 +3,138 @@
 
 
 
-#input the data of the function
-username = input(f"Please enter your name: ")
+#8.1 Define a function
+#username = input(f"Please enter your name: ") #input data 
+
+#def greet_user(username):
+#    """show a simple greeting"""
+#    print(f"Hello, {username.title()}!")
+
+#greet_user(username)  #call a function
 
 
-#define a function
-def greet_user(username):
-    """show a simple greeting"""
-    print(f"Hello, {username.title()}!")
+#8.2 Actual parameter and formal parameter
+#animal_type = input(f"Please enter the type of animal: ")
+#pet_name = input(f"Please enter the name of the pet: ")
+
+#def describe_pet(animal_type, pet_name):
+#    """display information about a pet"""
+#    print(f"\nI have a {animal_type}.")
+#    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+#describe_pet(animal_type, pet_name)
+
+#animal_type and pet_name are formal parameters, which are the variables that receive the values when the function is called.
+#the values you input are actual parameters, which are the values that you pass to a function when you call it.
+
+##8.2.1 Positional arguments 
+
+#def describe_pet(animal_type, pet_name):
+#    """showcase the information of the pet"""
+#    print("\nI have a " + animal_type + "'s name is " + pet_name.title() + ".")
+    
+#describe_pet("hamster", "harry")
+
+#hamster and harry are positional arguments
+#The order of the positional arguments have to match the order of formal parameter
+""" actual parameter 
+├── positional argument 
+└── keyword argument """
+
+##8.2.2 Keyword arguments
+
+#def describe_pet(animal_type, pet_name):
+#    """showcase the information of the pet"""
+#    print("\nI have a " + animal_type + "'s name is " + pet_name.title() + ".")
+    
+#describe_pet(animal_type = "hamster" , pet_name = "harry")
+
+#The datatype of the keyword argument is key-value pair
+#e.g. animal_type = "hamster", this is a key-value pair
+
+##8.2.3 default value
+
+#def describe_pet(pet_name = 'harry', animal_type):
+#    """showcase the information of the pet"""
+#    print("\nI have a " + animal_type + "'s name is " + pet_name.title() + ".")
+#def describe_pet(animal_type, pet_name = 'harry'):
+#    """showcase the information of the pet"""
+#    print("\nI have a " + animal_type + "'s name is " + pet_name.title() + ".")
+    
+#describe_pet(animal_type = "hamster")
+#describe_pet("hamster")
+
+#So if we don't use keyword argument, python will automatically sense the actual argument as position argument 
+#Believe it or not, you can check it using the two functions upwards
+#The order of keyword argument doesn't affect the output, but the order of the position argument can affect the output
+
+##8.2.4 common error
+#def describe_pet(animal_type, pet_name):
+    #"""showcase the information of the pet"""
+    #print("\nI have a " + animal_type + "'s name is " + pet_name.title() + ".")
+    
+#describe_pet()
+#describe_pet(animal_type = "dog")
+#describe_pet("Bobby","dog")
+
+#We are not going to pass any information to the formal argument
+#There is an error
+
+#8.3 return
+# y = f(x), the effect of "return" is to return the outcome of execution back to the function itself
+
+##8.3.1 return simple value
+
+#first_name = input(f"please input your first name: ")
+#last_name = input(f"please input your last name: ")
+
+#first_name, last_name = input(f"please input your first name: "), input(f"please input your last name: ")
+
+#def get_formatted_name(first_name, last_name):
+#    """return name"""
+#    return print(f'My name is {first_name.title()} {last_name.title()}')
+
+#get_formatted_name("Lawson", "Dong")
+#get_formatted_name(first_name, last_name)
+
+##8.3.2 let actual argument become optional
+#first_name, middle_name, last_name = input(f"please input your first name: "), input(f"please input your middle name: "), input(f"please input your last name: ")
+
+#def get_formatted_name(first_name, middle_name, last_name):
+#    """return name with optional middle name"""
+#    if middle_name:
+#        full_name =  f'My name is {first_name.title()} {middle_name.title()} {last_name.title()}'
+#    else:
+#        full_name = f'My name is {first_name.title()} {last_name.title()}'
+#    return print(full_name.title())
+       
+#get_formatted_name(first_name, middle_name, last_name)
+
+##8.3.3 return dictionary
+#first_name, last_name = input(f"please input your first name: "), input(f"please input your last name: ")
+
+#def get_formatted_name(first_name, last_name):
+#    """return a dictionary which containing the information of one's name"""
+#    person_name = {"first": first_name.title(), "last": last_name.title()}
+#    return person_name 
+
+#personal_information = get_formatted_name(first_name, last_name)
+
+#print(personal_information)
+#print(type(personal_information))
+
+#Using properties of the dictionary
+
+first_name, last_name, age = input(f"please input your first name: "), input(f"please input your last name: "), input(f"please input your age: ")
+def get_formatted_name(first_name, last_name, age = ""):
+    """return a dictionary with the age information of a person"""
+    person = {"first" : first_name, "last" : last_name}
+    if age:
+        person['age'] = age
+    return person
+
+personal_information = get_formatted_name(first_name, last_name, age)
+print(personal_information)
 
 
-greet_user(username)  #call a function
+
--- docs/code/README.md
+++ docs/code/README.md
@@ -8,5 +8,5 @@ Here are my code projects and GitHub links.
 
 - [Example Code](https://github.com/example) (Example link, actual links will be provided by you)
 - [SSH Key Configuration Guide](../SSH_key/README.md) - Detailed steps for GitHub SSH key configuration
-- [Python Classes Introduction](./Python%20basic/README.md) - Introduction to Python classes and object-oriented programming
+- [Python](./Python%20basic/README.md) - Introduction to Python classes and object-oriented programming
 - [PyTorch Tutorial](./pytorch使用教程/README.md) - PyTorch usage guide with machine learning model training examples
